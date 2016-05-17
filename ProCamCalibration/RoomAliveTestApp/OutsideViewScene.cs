5eec9b9d1d5d3ff680ec93f883f6b1d313a7eb8d
Progga <fenderandreas@web.de>
2016-05-17 10:42:00 +0200

    sharp graphics init

--- ProCamCalibration/RoomAliveTestApp/OutsideViewScene.cs
+++ ProCamCalibration/RoomAliveTestApp/OutsideViewScene.cs
@@ -1,5 +1,6 @@
 using RoomAliveTestApp;
 using RoomAliveToolkit;
+using SharpDX.Direct3D;
 using SharpGraphics;
 using System;
 using System.Collections.Generic;
@@ -34,6 +35,13 @@ namespace ProjectionMappingApp {
 			SharpDX.Matrix viewMat = cameraControl.getViewMatrix();
 			rMesh.meshShader.SetVertexShaderConstants(context,SharpDX.Matrix.Identity,viewMat*projMat,pointLight.position);
 			rMesh.meshShader.Render(context,rMesh.meshDeviceResources,pointLight,null,null,surface.viewport);
+
+			graphics.putPos(1,1,1);
+			graphics.putPos(-1,0.5f,1);
+			graphics.putIndex(0);
+			graphics.putIndex(1);
+			context.InputAssembler.PrimitiveTopology = PrimitiveTopology.LineList;
+			graphics.flush();
 		}
 
 		public override void RawEvent(InputEvent ev) {
--- ProCamCalibration/RoomAliveTestApp/SharpGraphics.cs
+++ ProCamCalibration/RoomAliveTestApp/SharpGraphics.cs
@@ -15,11 +15,18 @@ namespace SharpGraphics {
 
 		const int MAX_VERTICES = 100000;
 
-		public Vector3[] positions = new Vector3[MAX_VERTICES];
-		public int[] indices = new int[MAX_VERTICES*2];
+		private Vector3[] positions = new Vector3[MAX_VERTICES];
+		private int[] indices = new int[MAX_VERTICES*2];
+
+		private D3D11.Buffer vertexBuffer;
+		private D3D11.VertexBufferBinding vertexBinding;
+		private D3D11.Buffer indexBuffer;
 
 		D3DDevice device;
 		D3DDeviceContext context;
+
+		private int vertexPos = 0;
+		private int indexPos = 0;
 
 		private D3D11.DepthStencilState depthEnabledState;
 		private D3D11.DepthStencilState depthDisabledState;
@@ -27,6 +34,32 @@ namespace SharpGraphics {
 		public SharpGraphics(D3DDevice device) {
 			this.device = device;
 			this.context = device.ImmediateContext;
+
+			//--Init-buffers--
+			var vertexBufferDesc = new D3D11.BufferDescription() {
+				Usage = D3D11.ResourceUsage.Dynamic,
+				BindFlags = D3D11.BindFlags.VertexBuffer,
+				SizeInBytes = positions.Length,
+				CpuAccessFlags = D3D11.CpuAccessFlags.Write,
+				StructureByteStride = 0,
+				OptionFlags = 0,
+			};
+//			vertexBuffer = D3D11.Buffer.Create<Vector3>(device,D3D11.BindFlags.VertexBuffer,positions);
+			vertexBuffer = new D3D11.Buffer(device,vertexBufferDesc);
+			vertexBinding = new D3D11.VertexBufferBinding(vertexBuffer,Utilities.SizeOf<Vector3>(),0);
+
+			var indexBufferDesc = new D3D11.BufferDescription() {
+				Usage = D3D11.ResourceUsage.Dynamic,
+				BindFlags = D3D11.BindFlags.IndexBuffer,
+				SizeInBytes = indices.Length,
+				CpuAccessFlags = D3D11.CpuAccessFlags.Write,
+				StructureByteStride = 0,
+				OptionFlags = 0,
+			};
+			//indexBuffer = D3D11.Buffer.Create<int>(device,D3D11.BindFlags.IndexBuffer,indices);
+			indexBuffer = new D3D11.Buffer(device,indexBufferDesc);
+
+			//--Init-depth--
 			var depthStencilDesc = new D3D11.DepthStencilStateDescription {
 				DepthWriteMask = D3D11.DepthWriteMask.All,
 				DepthComparison = D3D11.Comparison.Less,
@@ -39,6 +72,46 @@ namespace SharpGraphics {
 
 		public void setDepthEnabled(bool enabled) {
 			context.OutputMerger.SetDepthStencilState(enabled ? depthEnabledState : depthDisabledState);
+		}
+
+		public void putPos(float x,float y,float z) {
+			positions[vertexPos++] = new Vector3(x,y,z);
+		}
+
+		public void putPos(Vector3 pos) {
+			positions[vertexPos++] = pos;
+		}
+
+		public void putIndex(int index) {
+			indices[indexPos++] = index;
+		}
+
+		public void putIndexTriangle(int index1,int index2,int index3) {
+			indices[indexPos++] = index1;
+			indices[indexPos++] = index2;
+			indices[indexPos++] = index3;
+		}
+
+		public void flush() {
+			SharpDX.DataStream dataStream;
+//			context.MapSubresource(psConstantBuffer,MapMode.WriteDiscard,MapFlags.None,out dataStream);
+//			dataStream.Write<T>(values);
+//			context.UnmapSubresource(psConstantBuffer,0);
+			context.MapSubresource(vertexBuffer,D3D11.MapMode.WriteDiscard,D3D11.MapFlags.None,out dataStream);
+			dataStream.WriteRange<Vector3>(positions,0,vertexPos);
+			context.UnmapSubresource(vertexBuffer,0);
+
+			context.MapSubresource(indexBuffer,D3D11.MapMode.WriteDiscard,D3D11.MapFlags.None,out dataStream);
+			dataStream.WriteRange<int>(indices,0,indexPos);
+			context.UnmapSubresource(indexBuffer,0);
+
+//			context.UpdateSubresource(positions,vertexBuffer);
+//			context.UpdateSubresource(indices,indexBuffer);
+
+			context.InputAssembler.SetVertexBuffers(0,vertexBinding);
+			context.InputAssembler.SetIndexBuffer(indexBuffer,SharpDX.DXGI.Format.R32_UInt,0);
+
+			context.DrawIndexed(indexPos,0,0);
 		}
 
 	}
